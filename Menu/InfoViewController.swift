23f1f610df13ec395a74c8d2f5fb049f8914e0b6
Cheng Xie <cheng_xie@brown.edu>
2015-03-30 19:58:32 -0400

    Tapping the image opens the URL

---
+++ Menu/InfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  InfoViewController.swift
+//  Waitless
+//
+//  Created by Cheng Xie on 3/30/15.
+//
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    @IBAction func imageTapped(sender: UITapGestureRecognizer) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "http://sakuraprovidence.com")!)
+    }
+}
